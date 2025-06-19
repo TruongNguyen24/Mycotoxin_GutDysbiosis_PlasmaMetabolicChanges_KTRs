1d1a01942340da509a96e09956c92d573cd80513
TruongNguyen24 <74937403+TruongNguyen24@users.noreply.github.com>
2025-06-19 11:39:57 +0200

    Create R_dependencies.R

---
+++ R_dependencies.R
@@ -0,0 +1,16 @@
+library(phyloseq)
+library(tidyverse)
+library(fido) # for Pibble model
+library(wec) # for weighted sum contrasts
+library(zCompositions) # for 0-imputation 
+library(driver) # for a few CoDa functions; devtools::install_github("jsilve24/driver")
+library(ggtext) # for plotting
+library(survival) # for survival analysis
+library(ggsurvfit) # for plotting KM curves
+library(survminer)
+library(codacore) # For balance prediction
+library(Hmisc)      # For rcorr() to compute correlation matrices
+library(igraph)
+library(reshape2)
+library(ggbreak) # for plotting
+library(ggpubr) # for plotting
